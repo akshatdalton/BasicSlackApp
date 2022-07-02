8fe91b430c9427a4e10b9d182c56ddce2767b33b
akshatdalton <akshat@markovml.com>
2022-07-02 14:54:51 +0530

    Add internal api to handle `Add to Slack` flow.
    Using the internal api we can directly associate
    the user account_id with their installation data.

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import os
+from urllib.parse import urljoin
 
 import uvicorn
 from fastapi import FastAPI, Request
 from slack_bolt import App
 from slack_bolt.adapter.fastapi import SlackRequestHandler
+from slack_bolt.oauth.oauth_settings import OAuthSettings
 from slack_sdk.oauth.installation_store import Installation, InstallationStore
+from starlette.datastructures import URL
 
 from fake_db import FakeDB
 
@@ -12,9 +15,24 @@ fake_db = FakeDB()
 
 
 class MyInstallationStore(InstallationStore):
+    def __init__(self) -> None:
+        self._account_id = None
+        super().__init__()
+
+    def set_account_id(self, account_id) -> None:
+        self._account_id = account_id
+
+    def _reset_account_id(self) -> None:
+        self._account_id = None
+
     def save(self, installation: Installation):
-        fake_db.set(installation.to_dict())
+        if self._account_id is None:
+            raise ValueError("Expected account_id")
+
+        fake_db.set(self._account_id, installation.to_dict())
+        print("account_id = ", self._account_id)
         print("installation = ", installation.to_dict())
+        self._reset_account_id()
 
 
 installation_store = MyInstallationStore()
@@ -24,6 +42,10 @@ app = App(
     raise_error_for_unhandled_request=True,
     signing_secret=os.environ["SLACK_SIGNING_SECRET"],
     installation_store=installation_store,
+    # Following `install_page_rendering_enabled` ensures that
+    # user doesn't have to click on `Add to Slack` button twice.
+    # They will be directly redirected to `https://slack.com/oauth/v2/authorize` page.
+    oauth_settings=OAuthSettings(install_page_rendering_enabled=False),
 )
 app_handler = SlackRequestHandler(app)
 
@@ -37,9 +59,32 @@ def custom_error_handler(error, body, logger):
 api = FastAPI()
 
 
-@api.get("/slack/install")
-async def install(req: Request):
-    return await app_handler.handle(req)
+############## BAD #############################################
+
+# @api.get("/slack/install")
+# async def install(req: Request):
+#     return await app_handler.handle(req)
+
+
+# @api.get("/start_auth_flow/{account_id}")
+# async def start_auth_flow(account_id: str):
+#     # Check if this `account_id` is valid or not.
+#     installation_store.set_account_id(account_id=account_id)
+#     RedirectResponse("/slack/install")
+
+
+########### GOOD ##############################################
+
+
+@api.get("/start_auth_flow/{account_id}")
+async def start_auth_flow(account_id: str, request: Request):
+    # Check if this `account_id` is valid or not.
+    installation_store.set_account_id(account_id=account_id)
+    # HACK: We don't want to expose `/slack/install` api since another user can
+    # directly hit it and save their installation data with different user `account_id`
+    # if this user started `App to Slack` flow but never completed it.
+    request._url = URL(urljoin(str(request.base_url), app.oauth_flow.install_path))
+    return await app_handler.handle(request)
 
 
 @api.get("/slack/oauth_redirect")
@@ -49,9 +94,10 @@ async def oauth_redirect(req: Request):
 
 @api.post("/messages")
 async def send_messages(message: str):
-    for token in fake_db.get_all():
+    for data in fake_db.get_all():
+        print("account_id = ", data["account_id"])
         result = app.client.chat_postMessage(
-            token=token, channel=os.environ["channel"], text=message
+            token=data["token"], channel=os.environ["channel"], text=message
         )
         print("result = ", result)
 
--- fake_db.py
+++ fake_db.py
@@ -1,8 +1,11 @@
 class FakeDB:
-    _db = []
+    _db = {}
 
-    def set(self, installation_dict):
-        self._db.append(installation_dict)
+    def set(self, account_id, installation_dict):
+        self._db[account_id] = installation_dict
 
     def get_all(self):
-        return [installation_dict["bot_token"] for installation_dict in self._db]
+        return [
+            dict(account_id=account_id, token=installation_dict["bot_token"])
+            for account_id, installation_dict in self._db.items()
+        ]
